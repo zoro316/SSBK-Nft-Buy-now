4b905213ea300ef7f6303ef7b7add337f55840d6
Filip Smolík <60159675+phil-more@users.noreply.github.com>
2021-10-06 20:47:08 +0200

    ntf

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const ASSET_URL = 'https://opensea.io/assets/0x1301566b3cb584e550a02d09562041ddc4989b91/28'
+export const ASSET_URL = 'https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/13381552457891204755844646832314136294320918458672548337252499977817320063045'
 
 export enum EmbedTheme {
   Default = "true",
